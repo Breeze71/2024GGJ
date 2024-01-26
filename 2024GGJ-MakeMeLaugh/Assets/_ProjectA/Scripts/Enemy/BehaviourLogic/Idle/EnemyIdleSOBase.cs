81f8e91f1efdf7e5f9034167a42ff924815f6206
V <j92080412@gmail.com>
2024-01-26 17:32:18 +0800

    Ai - Random Patrol

---
+++ 2024GGJ-MakeMeLaugh/Assets/_ProjectA/Scripts/Enemy/BehaviourLogic/Idle/EnemyIdleSOBase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace V
+{
+    public class EnemyIdleSOBase : ScriptableObject 
+    {
+        protected EnemyBase enemyBase;
+        protected Transform transform;  // SO 沒有 trasnform
+        protected GameObject gameObject;
+
+        protected Transform playerTransform;
+
+        public virtual void Initialize(GameObject _gameObject, EnemyBase _enemyBase)
+        {
+            gameObject = _gameObject;
+            transform = gameObject.transform;
+            enemyBase = _enemyBase;
+
+            playerTransform = enemyBase.PlayerTransform; 
+        }
+
+        public virtual void DoEnterState() {}
+        public virtual void DoExitState() { ResetValue(); }
+        public virtual void DoFrameUpdate() 
+        {
+
+        }
+        public virtual void DoPhysicsUpdate() {}
+        public virtual void DoAnimTriggerEvent(EnemyBase.AnimTriggerTypes _triggerTypes) {}
+        public virtual void ResetValue() {}
+    }
+}
