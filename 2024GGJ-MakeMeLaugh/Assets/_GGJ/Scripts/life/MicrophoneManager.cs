999f9935db9172629461acd7fdd8e1ff0477bcb7
Asphyxiala <119781076+Asphyxiala@users.noreply.github.com>
2024-01-27 03:09:10 +0800

    没做完：（

--- 2024GGJ-MakeMeLaugh/Assets/_GGJ/Scripts/life/MicrophoneManager.cs
+++ 2024GGJ-MakeMeLaugh/Assets/_GGJ/Scripts/life/MicrophoneManager.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
+using System.IO;
 using UnityEngine;
 using UnityEngine.iOS;
 using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
+
 public class MicrophoneManager : MonoBehaviour
 {
+
     public  float volume;
     private AudioSource aud;
     private String device;
@@ -18,12 +22,16 @@ public class MicrophoneManager : MonoBehaviour
     public float minY = 0.01f;
 
     private float ProCD;
-    private float ProCDTime;
+    private float ProCDTime = 0.01f;
     private float maxTime = 4.5f;
     private float closeCD;//保护机制关闭的CD
     private float closeCDTime = 0.3f;//保护机制关闭的CD时间
     private bool isStart;
     private bool reStart;//保护机制的开启判断
+
+    private int startPosition;
+    private int endPosition;
+
 
     [Header("语音识别")]
     private PhraseRecognizer phraseRecognizer;
@@ -47,16 +55,17 @@ public class MicrophoneManager : MonoBehaviour
         
         if(devices.Length > 0)
         {
-            isStart = true;
+
             device = devices[0];
             Debug.Log("找到麦克风");
         }
         else
         {
-            isStart = false;
+
             Debug.Log("NO MICROPHONE");
         }
 
+        isStart = false;
         aud.clip = Microphone.Start(device,true,999,1000);
  
     }
@@ -76,27 +85,37 @@ public class MicrophoneManager : MonoBehaviour
         }
         if((volume > minY)&&volume <= maxY)
         {
-            if(isStart)
+            
+            if(!isStart)
             {//此处留下判断，当在ProCD外还在说话才会开始录音
+                startPosition = Microphone.GetPosition(null);
+                Debug.Log("Y"+startPosition);
+                isStart = true;
+            }
+            if(isStart)
+            {
                 ProCD += Time.deltaTime;
                 if(ProCD >= ProCDTime)
                 {
                     if(ProCD <= (ProCDTime + maxTime))
                     {
-                        Debug.Log("开始录音");
+
                         closeCD = 0;
                         reStart = true;
+                        Debug.Log("开始录音");
 
                     }
                     else
                     {
                         ProCD = 0;
-                        isStart = true;
+                        isStart = false;
                         reStart = false;
                         //呼喊结束，写个方法进行下一步 保存录音
-                        SaveRecorder();
-                        Debug.Log("停止录音");
+                        endPosition = Microphone.GetPosition(null);
+                        Debug.Log(endPosition);
+                        SaveRecorder(startPosition,endPosition);
 
+                        Debug.Log("停止录音");
                     }
                 }
             }
@@ -112,11 +131,14 @@ public class MicrophoneManager : MonoBehaviour
                 {
                     ProCD = 0;
                     closeCD = 0;
-                    isStart = true;
+                    isStart = false;
                     reStart = false;
                     //呼喊中断，写个方法进行下一步 保存录音
-                    SaveRecorder();
+                    endPosition = Microphone.GetPosition(device);
+                    Debug.Log(endPosition);
+                    SaveRecorder(startPosition,endPosition);  
                     Debug.Log("停止录音");
+
                 }
             }
  
@@ -153,10 +175,21 @@ public class MicrophoneManager : MonoBehaviour
     /// <summary>
     /// 保存音频调用的方法
     /// </summary>
-    void SaveRecorder()
+    public void SaveRecorder(int startPosition,int endPosition)
     {
+        if (aud.clip == null)
+        {
+            Debug.LogError("AudioClip 为空。录制可能尚未开始。");
+            return;
+        }
+        byte[] wavData = WavUtility.FromAudioClip(aud.clip,startPosition,endPosition);
+        string fileName = "RecordedAudio_" + DateTime.UtcNow.ToString("yyyyMMdd_HHmmss") + ".wav";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
 
+        Debug.Log("音频已保存至：" + filePath);
+        
     }
+
     private void PhraseRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {  
         text.text = args.text;
